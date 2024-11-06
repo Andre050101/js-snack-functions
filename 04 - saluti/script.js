4fa56d4ce34c9a84b7bfbf70d14c485f5603929b
Andre050101 <agiancristiano90@gmail.com>
2024-11-06 17:33:00 +0100

    Exercise 4 done with arrow function too

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -3,13 +3,16 @@
 const name = 'Mario';
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui(Funzione tradizionale)
 function helloPerson(name){
     return "Ciao ["+name+"]";
 }
 
+// Dichiara la funzione qui(Arrow function)
+const hello = (name) => "Ciao ["+name+"]";
+
 // Invoca la funzione qui e stampa il risultato in console
 
 console.log(helloPerson(name));
-
+console.log(hello(name));
 //Risultato atteso se si passa 'Mario': // ciao Mario
