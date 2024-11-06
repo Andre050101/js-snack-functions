1b3ab02d9dcec01970f135c02c2da9d880751001
Andre050101 <agiancristiano90@gmail.com>
2024-11-06 17:29:21 +0100

    Exercise 3 done with arrow function too

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,13 @@
 
 const word = "Javascript";
 const vocalsArray = [];
-// Dichiara la funzione qui.
+// Dichiara la funzione qui(Funzione tradizionale)
 /**
  * Tale funzione prende in ingresso una qualunque parola e restituisce il numero di vocali presenti nella parola(Maiuscole o minuscole che siano)
  * @param {string} word 
  * @returns {number} counter
  */
+/*
 function vocalsCounter(word){
     let counter = 0;
     for(let i = 0; i < word.length; i++){
@@ -18,10 +19,25 @@ function vocalsCounter(word){
         }     
     }
     return counter;
+}*/
+// Dichiara la funzione qui(Arrow function)
+const vocalCountrFunction = (word) => {
+    const vocalsArray=[];
+    const vowels=['a', 'e', 'i', 'o', 'u'];
+    word.toLowerCase().split('').forEach(letter => {
+        if(vowels.includes(letter))
+            vocalsArray.push(letter);
+    });
+    
+    return {
+        count: vocalsArray.length,
+        vowels: vocalsArray
+    };
 }
 
-// Invoca la funzione qui e stampa il risultato in console
-console.log("Il numero di vocali nella parola passata è: "+vocalsCounter(word)+", le quali sono: "+vocalsArray);
 
+// Invoca la funzione qui e stampa il risultato in console
+//console.log("Il numero di vocali nella parola passata è: "+vocalsCounter(word)+", le quali sono: "+vocalsArray);
+console.log(vocalCountrFunction(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
