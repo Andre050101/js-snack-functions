a7abf7f9f5826d0eb0f0dc2f3f4a8493745db3ab
Andre050101 <agiancristiano90@gmail.com>
2024-11-06 16:16:54 +0100

    Exercise 2 done with arrow function

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+/**
+ * Funzione che prende in input un array ed una lettera e restituisce un array contenente solo parole che iniziano per quella lettera
+ * @param {Array} array 
+ * @param {String} letter 
+ * @returns Array 
+ */
+function filterArrayByLetter(array, letter){
+    return array.filter(word => word.startsWith(letter));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log("L'array formato dalle parole che iniziano per la lettera 'a' è: "+filterArrayByLetter(names, 'A'));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
