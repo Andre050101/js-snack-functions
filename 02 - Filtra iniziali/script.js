c43bfd456e49c8062c645966c0d0203efbbc7683
Andre050101 <agiancristiano90@gmail.com>
2024-11-06 17:14:05 +0100

    Exercise 2 done with traditional function

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -3,7 +3,7 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui(Arrow function)
 /**
  * Funzione che prende in input un array ed una lettera e restituisce un array contenente solo parole che iniziano per quella lettera
  * @param {Array} array 
@@ -13,8 +13,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 function filterArrayByLetter(array, letter){
     return array.filter(word => word.startsWith(letter));
 }
-
+// Dichiara la funzione qui(Funzione tradizionale)
+function filterArraybyLetter2(array, letter){
+    const finalArray=[];
+    for(let i = 0; i < array.length; i++){
+        let firstLetterLowered = array[i].charAt(0).toLowerCase(); 
+        if(firstLetterLowered === letter)
+            finalArray.push(array[i]);
+    }
+    return finalArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
 console.log("L'array formato dalle parole che iniziano per la lettera 'a' è: "+filterArrayByLetter(names, 'A'));
-
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
