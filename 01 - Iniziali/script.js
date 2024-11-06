d4cb8d052c9bc3e640fe67225441ab50df4dbb56
Andre050101 <agiancristiano90@gmail.com>
2024-11-06 15:37:33 +0100

    Exercise 1 done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+/**Funzione che prende in ingresso un array di stringhe e restituisce un array formato da iniziali delle stringhe appartenenti all'array in ingresso
+ * 
+ * @param {Array} array //array in ingresso dalla quale estrarre le iniziali 
+ * @returns {Array} firstLetter //Array contenente le lettere iniziali
+ */
+function firstLetterArray (array){              //Dichiarazione funzione firstLetterArray
+    const firstLetter = [];                     //Dichiarazione array che conterrà le lettere iniziali delle stringhe
+    for(let i = 0; i < array.length; i++){      //Dichiarazione ciclo for
+        firstLetter[i]=array[i].charAt(0);      //Per ogni elemento dell'array, estrae la prima lettera e la mette nell'array di iniziali 
+    }
+    return firstLetter;                         //Ritorna array con iniziali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log("L'array formato dalle lettere iniziali degli elementi appartenenti all'array di partenza è: "+firstLetterArray(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
